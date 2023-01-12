fc22b23c6a0c39194bab2015ff071e6c31e2d87b
Raulincher <93159054+Raulincher@users.noreply.github.com>
2023-01-12 11:40:13 +0100

    playerLife

---
+++ VR-Project/Assets/Scripts/playerLife.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerLife : MonoBehaviour
+{
+
+    public int maxHealth;
+    public int health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            health = maxHealth;
+        }
+    }
+}
